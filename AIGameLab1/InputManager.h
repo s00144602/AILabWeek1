9ba5f277b6bef450ab01bdd69f94151793302a64
shauna moffatt <shauna moffatt>
2016-09-30 11:37:35 +0100

    add files

---
+++ AIGameLab1/InputManager.h
@@ -0,0 +1,26 @@
+#include "stdafx.h" 
+#pragma comment(lib,"opengl32.lib") 
+#pragma comment(lib,"glu32.lib") 
+#include "SFML/Graphics.hpp" 
+#include "SFML/OpenGL.hpp" 
+#include <iostream> 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+class InputManager
+{
+//public:
+//	InputManager();
+//	~InputManager();
+//
+//public:
+//	bool bm_KeyHit(sf::Key::Code key);
+//	bool bm_KeyDown(sf::Key::Code key);
+//	int bm_MouseX();
+//	int bm_MouseY();
+//	void bm_init(sf::RenderWindow app);
+//private:
+//	sf::RenderWindow App;
+//	sf::Input& input = App.GetInput();
+};
+
